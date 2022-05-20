c6c9e26c5aaf8b5bf5168fb7d9f1dc48b25c1baf
Liza727 <simon.lizus@gmail.com>
2022-05-20 18:01:34 +0200

    cikkek 0520

--- cikkek.js
+++ cikkek.js
@@ -33,24 +33,18 @@ function feldolgoz_cim(cikkek_cim) {
         if (i != "id") {
                     
           if (i === "kep1"){
-            txt += "<div class='cimkep'><a href=\"#"+id+"\"><img src=\""+cikkekcim[i]+"\" width= 40%  max-height: 200px;></a></div>";
+            txt += "<div class='cimkep'><a href=\"#"+id+"\"><img src=\""+cikkekcim[i]+"\" width= auto  ></a></div>";
           }
-          /*txt += "<div class='foCim1'>"
-          if (i === "foCim1") {
-            txt += `<span class = "focim "> ${cikkekcim[i]}</span>`
-          }*/
-          /*else if (i === "bekezdes1") {
-            txt += `<span class = "bekezdes1"> ${cikkekcim[i]}</span>`
-          }*/
-          /*if (i === "szerzo"){
-            txt += `<span class = "szerzo"> ${cikkekcim[i]}</span>`
+          
+          else if (i === "foCim1"){
+            /*const lista=cikkekcim[i]; */
+            txt += `<h3 class ='focim'> ${cikkekcim[i]}</h3>`
           }
-          txt +="</div> ";
-          */
-          else if (i === "szoveg1"){
-            const lista=cikkekcim[i];          
-            for (const n in lista) {
-              txt += "<div class='cimszoveg'>"
+          else if (i === "szerzo"){
+              /*const lista=cikkekcim[i]; */
+              txt += `<span class = szerzo> ${cikkekcim[i]}</span>`         
+            /*for (const n in lista) {
+              
               if (n === "foCim1") {
                 txt += `<h3> ${lista[n]}</h3>`
               }
@@ -61,12 +55,13 @@ function feldolgoz_cim(cikkek_cim) {
               if (n === "szerzo"){
                 txt += `<span> ${lista[n]}</span>`
               }
-              txt +="</div> ";
+              
             }
+            txt +="</div> ";*/
           }
 
         }
-      }        
+      }
       txt +="</div> ";
     });
     txt +="</div> <br>";
@@ -76,43 +71,46 @@ function feldolgoz_cim(cikkek_cim) {
 
 function feldolgoz(cikkek) {
     var txt = " ";
-    var id='';  
+    var id='';
+    txt += "<div class='row2'>"
     cikkek.forEach(function  (cikkek){
-        //txt += "<div class='cikk'>"
+      txt += "<div class='cikk'>"
 
-    for (const i in cikkek) {
-      console.log(i);
-      if(i === "id"){
-        id=cikkek[i];
-      }
-      if (i != "id") {
-        if (i.indexOf("foCim")>=0) {
-          txt += `<h2 id=\"`+id+`\"> ${cikkek[i]}</h2>`
+      for (const i in cikkek) {
+        console.log(i);
+        if(i === "id"){
+          id=cikkek[i];
         }
-        else if (i.indexOf("cim")>=0){
-          txt += `<h3> ${cikkek[i]}</h3>`
-        }
-        else if (i.indexOf("bekezdes")>=0 /* === "bekezdes1"  || i === "bekezdes2"  || i === "bekezdes3"  || i === "bekezdes4"  || i === "bekezdes5"  || i === "bekezdes6"  || i === "bekezdes7"  || i === "bekezdes8"  || i === "bekezdes9"  || i === "bekezdes10"  || i === "bekezdes11"  || i === "bekezdes112"  || i === "bekezdes13"  || i === "bekezdes14"  || i === "bekezdes15"  || i === "bekezdes16"  || i === "bekezdes17"  || i === "bekezdes18"  || i === "bekezdes19"*/){
-          txt += `<span> ${cikkek[i]}</span><br>`
-        }
-        else if (i === "link1"){
-          txt += `<span> ${cikkek[i]}</span>`
-        }
-        else if (i === "szerzo"){
-          txt += `<span> ${cikkek[i]}</span>`
-        }
-        else if (i.indexOf("kep")>=0){
-          txt += "<img src=\""+cikkek[i]+"\" width= 100px height= 100 px><br>";
-        }
-        else if (i.indexOf("fels")>=0){
-          const lista=cikkek[i];          
-          for (const n in lista) {
-            txt += "<li><span>"+lista[n]+"</span>";
+        if (i != "id") {
+          if (i.indexOf("foCim")>=0) {
+            txt += `<h2 id=\"`+id+`\"> ${cikkek[i]}</h2>`
+          }
+          else if (i.indexOf("cim")>=0){
+            txt += `<h3> ${cikkek[i]}</h3>`
+          }
+          else if (i.indexOf("bekezdes")>=0 ){
+            txt += `<span> ${cikkek[i]}</span><br>`
+          }
+          else if (i === "link1"){
+            txt += `<span> ${cikkek[i]}</span>`
+          }
+          else if (i === "szerzo"){
+            txt += `<span> ${cikkek[i]}</span>`
+          }
+          else if (i.indexOf("kep")>=0){
+            txt += "<img src=\""+cikkek[i]+"\" width= 100px height= 100 px><br>";
+          }
+          else if (i.indexOf("fels")>=0){
+            const lista=cikkek[i];          
+            for (const n in lista) {
+              txt += "<li><span>"+lista[n]+"</span>";
+            }
           }
         }
       }
-        }        
+      txt +="</div> ";
     });
+    txt +="</div> <br>";
     console.log(txt)
     $('article')[0].innerHTML += txt;
 
@@ -120,10 +118,10 @@ function feldolgoz(cikkek) {
 
     var i;
 
-    /*for (i = 0; i < elements.length; i++) {
-      elements[i].style.msFlex = "30%";  // IE10
-      elements[i].style.flex = "30%";
+    for (i = 0; i < elements.length; i++) {
+      elements[i].style.msFlex = "45%";  // IE10
+      elements[i].style.flex = "45%";
     }
-    */
+    
     
 }
