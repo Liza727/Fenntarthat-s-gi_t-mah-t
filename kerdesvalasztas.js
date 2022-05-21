6398f2a6b6c84d9fa167855dc4718b4881b7584e
Bgeri8791 <abc@example.com>
2022-05-21 08:56:59 +0200

    20220521_aprostyle

--- kerdesvalasztas.js
+++ kerdesvalasztas.js
@@ -3,15 +3,28 @@ function $(elem) {
     return document.querySelectorAll(elem)
   }
 
+  function vonalbetolt() {
+    var vonal = document.getElementsByClassName("Title2");
+    for (let i = 0; i < vonal.length; i++) {
+      vonal[i].style.borderBottom = "5px solid #30A0AD"
+      
+    }
+
+  }
+
 function init(){
+  
   $("#adult")[0].addEventListener("click",function () {
     kerdesbetoltes("adultkerdesek.json");
+    vonalbetolt();
   });
   $("#mschool")[0].addEventListener("click",function () {
     kerdesbetoltes("middlechoolkerdesek.json");
+    vonalbetolt();
   });
   $("#hgschool")[0].addEventListener("click",function () {
     kerdesbetoltes("hightschoolkerdesek.json");
+    vonalbetolt();
   });  
 }
 
